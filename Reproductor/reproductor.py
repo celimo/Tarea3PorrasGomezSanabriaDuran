9c188b70eed44d601064b64911d9189dad7ce098
Celimo Porras Aguilar <68404113+celimo@users.noreply.github.com>
2020-10-14 00:20:02 -0600

    Actualización reproductor (tiempo)
    Se agrega el tiempo de ejecución del método y se imprime al final

--- Reproductor/reproductor.py
+++ Reproductor/reproductor.py
@@ -11,9 +11,10 @@ import time
 # Local de la función reproducirAudio
 # nombre / Contiene la dirección de la canción a reproducir
 # cant / Indica cuantas veces se debe reproducir la canción
+# t1 / Indica el tiempo inicial en que se empezó a ejecutar el método
 
 # Función para reproducir un sonido
-def reproducirAudio(nombre, cant):
+def reproducirAudio(nombre, cant, t1)):
 	mixer.init()
 	mixer.music.load(nombre)  # Se carga el archivo con la dirección
 	mixer.music.play(cant)  # Se inicia la reproducción cierta cantidad
@@ -23,11 +24,14 @@ def reproducirAudio(nombre, cant):
 			while mixer.music.get_busy():  # Solo sirve para que no se cierre el programa
 				time.time()
 			cant -= 1
+	t2=time.time() - t1
+	print("Tiempo de ejecucion:", t2, "segundos")
 
 # Parte del argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("nombre", help ="Nombre del archivo .mp3", type=str)
 parser.add_argument("cant", help ="Cantidad de reproducciones de la canción", type=int)
+parser.add_argument("--time", default=time.time())
 args = parser.parse_args()
 
-reproducirAudio(args.nombre, args.cant)
+reproducirAudio(args.nombre, args.cant, args.time)
