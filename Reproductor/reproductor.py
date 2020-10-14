ee07d0ff6bf3b204730702538393ac93da1fafae
Celimo Porras Aguilar <68404113+celimo@users.noreply.github.com>
2020-10-13 23:38:21 -0600

    Corrección de errores repoductor.py
    Contenía algunos errores e indicar la cantidad de reproducciones desde el play era inestable
    Se agregaron comentarios al código

--- Reproductor/reproductor.py
+++ Reproductor/reproductor.py
@@ -1,14 +1,28 @@
+# Programa para reproducir un audio indicando la ubicación del archivo y la cantidad de veces que se desea reproducir
+
 #Librerias
 import argparse
 from pygame import mixer
+import time
 
+# Variables
+# Globales
+# args / Objeto con los parámetros que se indican al ejecutar el programa
+# Local de la función reproducirAudio
+# nombre / Contiene la dirección de la canción a reproducir
+# cant / Indica cuantas veces se debe reproducir la canción
 
 # Función para reproducir un sonido
 def reproducirAudio(nombre, cant):
 	mixer.init()
 	mixer.music.load(nombre)  # Se carga el archivo con la dirección
 	mixer.music.play(cant)  # Se inicia la reproducción cierta cantidad
-	while mixer.music.get_busy():
+	if(cant > 0):  # Siempre que la cantidad sea mayor que cero se reproduce
+		while (cant > 0): 
+			mixer.music.play()  # Se inicia la reproducción cierta cantidad
+			while mixer.music.get_busy():  # Solo sirve para que no se cierre el programa
+				time.time()
+			cant -= 1
 
 # Parte del argparse
 parser = argparse.ArgumentParser()
@@ -16,4 +30,4 @@ parser.add_argument("nombre", help ="Nombre del archivo .mp3", type=str)
 parser.add_argument("cant", help ="Cantidad de reproducciones de la canción", type=int)
 args = parser.parse_args()
 
-funcion = reproducirAudio(args.nombre, args.cant)
+reproducirAudio(args.nombre, args.cant)
